2b9f20cfc3105ec51039ff187a98d8a717b2d10a
unknown <juansemedina30@gmail.com>
2023-10-19 00:58:13 -0500

    Merge corrections

--- src/main/java/com/example/securityhome/util/Authentication.java
+++ src/main/java/com/example/securityhome/util/Authentication.java
@@ -3,12 +3,8 @@ package com.example.securityhome.util;
 import com.example.securityhome.model.Repository.UserRepository;
 import com.example.securityhome.model.entity.User;
 import com.example.securityhome.model.entitydto.UserDTO;
-<<<<<<< HEAD
 import com.example.securityhome.model.entitydto.UserLoginDTO;
-=======
->>>>>>> 37d4a9516a10fc4b9b6ec1b5f38399c97c8a1022
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,7 +18,6 @@ public class Authentication {
     }
 
 
-<<<<<<< HEAD
     public User isValid(UserLoginDTO user){
         var entityUser=repository.getUserByEmail(user.getE());
         try{
@@ -37,28 +32,5 @@ public class Authentication {
 
     public User findByUUID(String uuid){
         return (User) repository.findById(uuid).orElse(null);
-=======
-    public User isValid(UserDTO user){
-        var users=repository.findAll();
-        User registeredUser;
-        for (User userE : users){
-            if(user.getE().equals(userE.getEmail())){
-                if (user.getP().equals(userE.getPassword())){
-                    return userE;
-                }
-            }
-        }
-        return null;
-    }
-
-    public User findByUUID(String uuid){
-        var users=repository.findAll();
-        for (User userE : users){
-            if(uuid.equals(userE.getId())){
-                return userE;
-            }
-        }
-        return null;
->>>>>>> 37d4a9516a10fc4b9b6ec1b5f38399c97c8a1022
     }
 }
