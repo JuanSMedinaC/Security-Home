dc765a5cb3d7e8a0e4dae8f4651553a30e54688a
Juan Yustes <yustes2017@gmail.com>
2023-11-15 22:29:53 -0500

    Added endpoint(back) and show HTML(Front)

---
+++ frontend/scripts/Historial.js
@@ -0,0 +1,29 @@
+const alarmCardContainer = document.getElementById('alarmCardContainer');
+
+var parse = JSON.parse(window.localStorage.getItem('Authorization'));
+
+async function getAlerts(){
+    let auth = localStorage.getItem("Authorization");
+    let response = await fetch('http://localhost:8080/alert/all',{
+        method: 'GET',
+        headers: {
+            'Authorization':"123"
+        },
+    });
+
+    if(response.status ===200){
+        let json = await response.json();
+        console.log(json);
+
+        json.forEach(alert => {
+        var card = new AlertCard( alert );
+        console.log( card.render() );
+        console.log(alertCardContainer);
+        alertCardContainer.appendChild(card.render());    
+        });
+    } else {
+        alert(await response.text());
+    }
+}
+
+getAlerts();
